3d028489cc3abcac98c9f7b3e0bee3a4c8c04050
MingTone <github@mtmail.xyz>
2026-02-17 17:31:57 +0800

    Remove Windows-specific memory and socket linger implementations
    - Deleted `mem_windows.go` and `mem_windows_stub.go` which provided memory limit functionality for Windows and a stub for non-Windows platforms.
    - Removed `sock_linger_unix.go` and `sock_linger_windows.go` that handled socket linger settings for Unix and Windows respectively.

--- fd_limit_windows.go
+++
@@ -1,9 +0,0 @@
-//go:build windows
-// +build windows
-
-package main
-
-// detectFDLimit returns a conservative default on Windows.
-func detectFDLimit() uint64 {
-	return 8192
-}
--- mem_windows_stub.go
+++
@@ -1,9 +0,0 @@
-//go:build !windows
-// +build !windows
-
-package main
-
-// windowsMemLimit reports 0 on non-Windows platforms.
-func windowsMemLimit() int64 {
-	return 0
-}
